b58991de39c2fa01cfc1ed1544a9c48aaac0de54
trkea <trs1141@yahoo.co.jp>
2018-12-01 20:27:46 +0900

    code more simply

--- api/re_api.php
+++ api/re_api.php
@@ -1,15 +1,11 @@
 <?php
-$selectName = $_GET['name'];
 $enzymeJson = file_get_contents('restriction_enzymes.json');
-$enzymeJson = mb_convert_encoding($enzymeJson, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-$obj = json_decode($enzymeJson, true);
-$resultNum = array_search($selectName, array_column($obj, 'name'));
+$obj = json_decode($enzymeJson, true); //string => arrayへ変換する
+$resultNum = array_search($_GET['name'], array_column($obj, 'name')); //arrayから検索する
 if($resultNum != False) {
-	$result = $obj[$resultNum];
-    echo json_encode($result);
+    echo json_encode($obj[$resultNum]);
 }
 else {
-	$result = array('name' => 'Not Invalid');
-	echo json_encode(($result));
+	echo json_encode(array('name' => 'Not Invalid'));
 }
 ?>
